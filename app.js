2f1d7f4d6259d1ea857ecb4d17ae912504a392e3
David <drd.087@gmail.com>
2018-10-30 15:38:33 -0700

    Checkpoint 6 Scripting: Completed

--- app.js
+++ app.js
@@ -1,71 +1,47 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+ const toDos = [];
+ const addToDoForm = document.getElementById('addToDoForm');
 
-   addToDoForm.addEventListener('submit', event => {
-   event.preventDefault();
+ function createNewToDo() {
+   const newToDoText = document.getElementById('newToDoText');
+   if (!newToDoText.value) { return; }
 
-   // get the text
-   let title = newToDoText.value;
+   toDos.push({
+     title: newToDoText.value,
+     complete: false
+   });
 
-   // create a new li with specified class
-   let newLi = document.createElement('li');
-   newLi.setAttribute("class", "mdl-grid mdl-cell mdl-cell--12-col mdl-cell--8-col-tablet mdl-cell--4-col-phone mdl-grid--no-spacing mdl-card mdl-shadow--2dp");
-   newLi.setAttribute("id", "contain");
-
-   // attach the li to the ul
-   toDoList.appendChild(newLi);
-
-   // create a new input(checkbox) within a div
-   let checkbox = document.createElement('div');
-   checkbox.setAttribute("class", "mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--1-col-phone mdl-grid--no-spacing checkdiv");
-   checkbox.innerHTML = "<input type = 'checkbox' class = 'checktask'>";
-
-   // attach the checkbox to the li
-   newLi.appendChild(checkbox);
-
-   //create a div within the li
-   let newDiv = document.createElement('div');
-   newDiv.setAttribute('class', 'mdl-cell mdl-cell--9-col mdl-cell--5-col-tablet mdl-cell--2-col-phone mdl-grid--no-spacing lidiv');
-   newLi.appendChild(newDiv);
-
-   // set the title
-   newDiv.textContent = title;
-
-   //create placeholder for the delete box
-
-   let deldiv = document.createElement('div');
-   deldiv.setAttribute("class", "mdl-cell mdl-cell--2-col mdl-cell--2-col-tablet mdl-cell--1-col-phone mdl-grid--no-spacing delhold");
-
-   // attach the remove option to the li
-   newLi.appendChild(deldiv);
-
-   // delete box img
-   let removeLi = document.createElement('img');
-   removeLi.src = "./img/baseline-delete_outline-24px.svg"
-   removeLi.setAttribute("class", "delimg");
-   removeLi.setAttribute("alt", "Delete Task");
-
-   //del box identifier
-   let deltask= document.createTextNode("Delete");
-
-   //attach the identifier to the button
-   removeLi.appendChild(deltask);
-
-   //attach the button to the deldiv
-   deldiv.appendChild(removeLi);
-
-   //empty the input
    newToDoText.value = '';
 
-   // event listener for deleting
-    removeLi.addEventListener('click', function() {
-      newLi.parentNode.removeChild(newLi);
-    })
+   renderTheUI();
+   }
+
+ function renderTheUI() {
+   const toDoList = document.getElementById('toDoList');
+
+   toDoList.textContent = '';
+
+   toDos.forEach(function(toDo) {
+     const newLi = document.createElement('li');
+     const checkbox = document.createElement('input');
+     checkbox.type = "checkbox";
+
+     newLi.textContent = toDo.title;
+
+     toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+   });
+
+ }
+
+ addToDoForm.addEventListener('submit', event => {
+   event.preventDefault();
+   createNewToDo();
  });
+
+ renderTheUI();
 }
 
 window.onload = function() {
-    onReady();
+  onReady();
 };
