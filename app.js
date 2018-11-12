90d0978040acac284629d0afdd137217280f50df
David <drd.087@gmail.com>
2018-11-12 15:07:48 -0800

    Assignment 6: Completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function onReady() {
- const toDos = [];
+ let id = 0;
+ let toDos = [];
  const addToDoForm = document.getElementById('addToDoForm');
 
  function createNewToDo() {
@@ -8,13 +9,22 @@ function onReady() {
 
    toDos.push({
      title: newToDoText.value,
-     complete: false
+     complete: false,
+     id: ++id
    });
 
    newToDoText.value = '';
 
    renderTheUI();
    }
+
+/*delete function, executed by the event listener. Compares the id
+property of each toDo, and through the filter method, a new array is created,
+returning values meeting the conditions given
+*/
+ function deleteLi(id) {
+   return toDos.filter(toDo => toDo.id !== id);
+ }
 
  function renderTheUI() {
    const toDoList = document.getElementById('toDoList');
@@ -26,13 +36,25 @@ function onReady() {
      const checkbox = document.createElement('input');
      checkbox.type = "checkbox";
 
+ //create a delete button
+     const del = document.createElement('button');
+     del.innerHTML = '<span>Delete</span>';
+
      newLi.textContent = toDo.title;
 
      toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
-   });
+//append the Delete button to the li
+     newLi.appendChild(del);
 
- }
+//event listener for the user clicking delete
+    del.addEventListener('click', event => {
+      toDos = deleteLi(toDo.id);
+      renderTheUI();
+    });
+   })
+
+ };
 
  addToDoForm.addEventListener('submit', event => {
    event.preventDefault();
